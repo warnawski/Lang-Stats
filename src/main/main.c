57142414b3857ba58d42a5c7cc7d0d3c2f474780
Sergey Warnawski <ilsergdm@gmail.com>
2025-09-08 00:42:53 +0300

    feach: add new command(version, help, fetch)

--- src/main/main.c
+++ src/main/main.c
@@ -3,6 +3,9 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+
+#define CLI_VERSION "0.0.1"
 
 int main(int argc, char *argv[])
 {
@@ -10,5 +13,20 @@ int main(int argc, char *argv[])
     {
         perror("Wrong number of arguments");
         return 1;
+    }
+
+    if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)
+    {
+        printf("");
+        return 0;
+    }
+
+    else if (strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0)
+    {
+        printf("LangStat Version: %s\n", CLI_VERSION);
+    }
+    else if (strcmp(argv[1], "--fetch") == 0 || strcmp(argv[1], "--f") == 0)
+    {
+
     }
 }
