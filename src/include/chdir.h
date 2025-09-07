3a0fe9411248584ef439d8c6dd638bdb5ee3d30c
Sergey Warnawski <ilsergdm@gmail.com>
2025-09-08 01:08:30 +0300

    feach: add .h lib for create cross platforming cli in the future

---
+++ src/include/chdir.h
@@ -0,0 +1,10 @@
+//
+// Created by warnawski on 08.09.2025.
+//
+
+#ifndef PROJECT_STATS_CHDIR_H
+#define PROJECT_STATS_CHDIR_H
+
+int get_current_directory(char *buffer, size_t size);
+
+#endif //PROJECT_STATS_CHDIR_H
