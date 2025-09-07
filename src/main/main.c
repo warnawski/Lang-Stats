df3022451c6cb4e769a387a9b998acfdb318d3c3
Sergey Warnawski <ilsergdm@gmail.com>
2025-09-08 00:09:46 +0300

    feach: create base main function

---
+++ src/main/main.c
@@ -0,0 +1,23 @@
+//
+// Created by warnawski on 07.09.2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        perror("Wrong number of arguments");
+        return 1;
+    }
+
+    switch (*argv[1])
+    {
+        case '--help':
+            printf("");
+            return 0;
+
+
+    }
+}
