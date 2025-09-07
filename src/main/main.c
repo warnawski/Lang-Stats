63dfdfc291b935054c7cfcfef5c8d21e42214afa
Sergey Warnawski <ilsergdm@gmail.com>
2025-09-08 01:09:23 +0300

    chor: new realization command '--fetch'

--- src/main/main.c
+++ src/main/main.c
@@ -3,7 +3,10 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include "../include/chdir.h"
+
 
 #define CLI_VERSION "0.0.1"
 
@@ -27,6 +30,20 @@ int main(int argc, char *argv[])
     }
     else if (strcmp(argv[1], "--fetch") == 0 || strcmp(argv[1], "--f") == 0)
     {
-
+        char *buffer = malloc(4096);
+        if (!buffer)
+        {
+            perror("malloc");
+            exit(1);
+        }
+        get_current_directory(buffer, 4096);
+        printf("VOT %s\n", buffer);
+        free(buffer);
+        return 0;
+    }
+    else
+    {
+        perror("Unknown command, please send command '--help'");
+        return 1;
     }
 }
