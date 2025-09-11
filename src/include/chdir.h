9f1e42dcb4a13ac2855a07baa085c930c5494612
Sergey Warnawski <ilsergdm@gmail.com>
2025-09-12 00:44:55 +0300

    feach: delete overhead depends from function operations with directory and files

--- src/include/chdir.h
+++ src/include/chdir.h
@@ -4,7 +4,10 @@
 
 #ifndef PROJECT_STATS_CHDIR_H
 #define PROJECT_STATS_CHDIR_H
+#include "../include/lstat.h"
 
 int get_current_directory(char *buffer, size_t size);
+
+int directory_traversal();
 
 #endif //PROJECT_STATS_CHDIR_H
