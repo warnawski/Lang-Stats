7bfb52b58a18c9fe753b51ee164bc795dbeeb2f7
Sergey Warnawski <ilsergdm@gmail.com>
2025-09-12 23:12:26 +0300

    feach: add new languages and lang-extensions

--- src/internal/base_lang.c
+++ src/internal/base_lang.c
@@ -17,11 +17,24 @@ void init_all_lang(const char *ext[], const char *langs[], size_t count)
 void init_st()
 {
     const char *ext[] = {
-        "c", "py", "go", "cpp", "js", "rs", "h", "java", "cs", "php", "sh", "hpp", "clj", "cr", "dart", "pas", "ex"
+        "c", "py", "go", "cpp", "js", "rs", "h", "java", "cs", "php", "sh", "hpp", "clj", "cr", "dart", "pas", "ex",
+        "swift", "kt", "m", "mm", "rb", "ts", "scala", "vb", "pl", "pm", "r", "jl", "lua", "hs", "lhs", "fs", "fsi",
+        "fsscript", "ml", "mli", "erl", "hrl", "exs", "scm", "ss", "lisp", "el", "awk", "tcl", "ada", "d", "nim",
+        "zig", "vala", "vbnet", "asm", "s", "v", "sv", "vhd", "sql", "prc", "ada", "cbl", "f", "f90", "for", "bas",
+        "m4", "groovy", "gradle", "hx", "pro", "cl", "idl", "rego", "cue", "ipynb", "md", "tex", "bib", "xml", "yml",
+        "yaml", "json", "toml", "ini"
     };
 
     const char *langs[] = {
-        "C", "Python", "Go", "C++", "JavaScript", "Rust", "Header C file", "Java", "C#", "PHP", "Bash", "Header C++ file" , "Clojure", "Crystal", "Dart", "Pascal", "Elexir"
+        "C", "Python", "Go", "C++", "JavaScript", "Rust", "Header C file", "Java", "C#", "PHP", "Bash", "Header C++ file",
+        "Clojure", "Crystal", "Dart", "Pascal", "Elixir", "Swift", "Kotlin", "Objective-C", "Objective-C++",
+        "Ruby", "TypeScript", "Scala", "Visual Basic", "Perl", "Perl Module", "R", "Julia", "Lua", "Haskell", "Literate Haskell",
+        "F#", "F# Interface", "F# Script", "OCaml", "OCaml Interface", "Erlang", "Erlang Header", "Elixir Script", "Scheme",
+        "Scheme Script", "Common Lisp", "Emacs Lisp", "Awk", "Tcl", "Ada", "D", "Nim", "Zig", "Vala", "VB.NET",
+        "Assembly", "Assembly (AT&T/Intel)", "Verilog", "SystemVerilog", "VHDL", "SQL", "Procedural SQL", "Ada (dup)",
+        "COBOL", "Fortran", "Fortran 90", "Fortran (legacy)", "BASIC", "M4 Macro", "Groovy", "Gradle Script", "Haxe",
+        "Prolog", "OpenCL", "IDL", "Rego", "CUE", "Jupyter Notebook", "Markdown", "LaTeX", "BibTeX", "XML",
+        "YAML", "YAML", "JSON", "TOML", "INI"
     };
 
     size_t count = sizeof(ext) / sizeof(ext[0]);
